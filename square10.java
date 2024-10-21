89b9acbc7ec0472af065954017f8aed5caf02bf3
rahma <fijriatirahmaturrizqi@gmail.com>
2024-10-21 20:29:27 +0700

    percobaan 1 dan 2

---
+++ square10.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class square10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai N = ");
+        int N = sc.nextInt();
+        for (int iOuter = 1; iOuter <= N; iOuter++) {
+            for (int iInner = 0; iInner <= N; iInner++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
